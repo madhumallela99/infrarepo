62d7436097f78f5b5b6e062543c9d9ae37a07c98
root <root@ip-172-31-34-16.ap-south-1.compute.internal>
2026-02-19 12:26:47 +0000

    new files added

--- eks.tf
+++ eks.tf
@@ -1,19 +1,20 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.4"
+  version = "19.21.0"
 
   cluster_name    = "springboot-eks"
   cluster_version = "1.29"
 
-  subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 3
       instance_types = ["t3.medium"]
     }
   }
 }
+
