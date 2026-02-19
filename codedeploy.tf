2786ab70a0822aa9058b2eb0dda1e8815c351fc1
madhumallela99 <151830395+madhumallela99@users.noreply.github.com>
2026-02-19 18:47:21 +0530

    Update codedeploy.tf

--- codedeploy.tf
+++ codedeploy.tf
@@ -1,4 +1,18 @@
+############################################
+# CodeDeploy Application
+############################################
+
+resource "aws_codedeploy_app" "eks_app" {
+  name             = "springboot-eks-app"
+  compute_platform = "Server"
+}
+
+############################################
+# CodeDeploy Deployment Group
+############################################
+
 resource "aws_codedeploy_deployment_group" "eks_dg" {
+
   app_name              = aws_codedeploy_app.eks_app.name
   deployment_group_name = "springboot-dg"
   service_role_arn      = aws_iam_role.codedeploy_role.arn
@@ -20,5 +34,3 @@ resource "aws_codedeploy_deployment_group" "eks_dg" {
     }
   }
 }
-
-
